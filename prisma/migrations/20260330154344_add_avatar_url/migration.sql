7e064ec292c8dfa756d6344d3fa70e80fb0ae27a
Abraham Miranda <102253410+Skycosterz@users.noreply.github.com>
2026-04-03 14:40:24 -0600

    v1

--- prisma/migrations/20260330154344_add_avatar_url/migration.sql
+++
@@ -1,6 +0,0 @@
--- Avatar Engine migration: add avatarUrl to Character
--- This is the only column we actually need to add. The Story table fields
--- (genre, scenario, characterIds) already exist in the database from a
--- prior migration, so we skip touching that table here.
-
-ALTER TABLE "Character" ADD COLUMN "avatarUrl" TEXT;
--- prisma/migrations/20260330185802_character_consistency/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Character" ADD COLUMN "canonicalDescription" TEXT;
-ALTER TABLE "Character" ADD COLUMN "negativePrompt" TEXT;
-ALTER TABLE "Character" ADD COLUMN "seed" INTEGER;
-ALTER TABLE "Character" ADD COLUMN "styleProfile" TEXT;
-ALTER TABLE "Character" ADD COLUMN "visualTraits" TEXT;
